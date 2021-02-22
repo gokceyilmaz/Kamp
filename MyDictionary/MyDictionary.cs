ca1d3de784ce9d60edcd242edcd6bd69ba4fe50e
Gökçe Yılmaz <gokceeyy@hotmail.com>
2021-02-22 20:50:33 +0300

    Dictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<K, V>
+    {
+        K[] key;
+        V[] value;
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+        public void Add(K keys, V values)
+        {
+            K[] tempK = key;
+            V[] tempV = value;
+
+            key = new K[key.Length + 1];
+            value = new V[value.Length + 1];
+
+            for (int i = 0; i < tempK.Length; i++)
+            {
+                key[i] = tempK[i];
+            }
+            key[key.Length - 1] = keys;
+
+            for (int i = 0; i < tempV.Length; i++)
+            {
+                value[i] = tempV[i];
+            }
+            
+            value[value.Length - 1] = values;
+        }
+        
+        public void List()
+        {
+            for (int i= 0; i<key.Length;i++)
+            {
+                Console.WriteLine("No: " + key[i]+" " + "İsim: "+ value[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> people = new  MyDictionary<int, string>();
+            
+            people.Add (1, "Gökçe");
+            people.Add (2, "Burak");
+            people.Add(3, "Gözde");
+            people.Add(4, "Rüzgar");
+
+            
+           people.List();
+
+        }
+    }
+    
+}
